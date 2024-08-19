48cf9c8416c25b4068d1501231cb39f7075e2e52
gabrielaspooky <gabrielanava01@gmail.com>
2024-08-19 13:00:45 +0200

    added new info

--- cafe-de-altura/src/app/success/page.jsx
+++ cafe-de-altura/src/app/success/page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ClipboardCheck } from "lucide-react";
+import Link from "next/link";
 
 const success = () => {
   return (
@@ -11,6 +12,10 @@ El pedido #12387 se encuentra en preparación.
 Lo recibirás dentro de las fechas acordadas en tu envío. 
 Hemos enviado un ticket a tu correo electrónico.
 </p>
+
+<Link href="/shop" className="bg-[#2A5B45] hover:bg-[#505050] text-white text-sm py-2 px-4 rounded-lg">
+          Volver a la tienda
+        </Link>
     </div>
   )
 }
